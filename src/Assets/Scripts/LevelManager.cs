e8d8a1290e4fc8a14ebb32e779d4af6e984a91be
angelinn <angelin.nedelchev@outlook.com>
2018-11-09 21:51:07 +0200

    Change font on menu scene and add button sound effect.

--- src/Assets/Scripts/LevelManager.cs
+++ src/Assets/Scripts/LevelManager.cs
@@ -3,19 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour {
-    
+public class LevelManager : MonoBehaviour
+{
     public void ChangeLevel(int level)
     {
+        SoundManager.Instance.PlayAudio("ButtonPress");
         SceneManager.LoadScene(level);
     }
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
